5478f6bfa2c89f0be79e6b732864e91c953a89ce
ahmaadyunus <scootyunus@gmail.com>
2016-12-15 11:22:21 +0700

    First Commit

---
+++ app/src/main/java/com/example/ahmaadyunus/retrofit/UserApi.java
@@ -0,0 +1,40 @@
+package com.example.ahmaadyunus.retrofit;
+
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.DELETE;
+import retrofit2.http.GET;
+import retrofit2.http.POST;
+import retrofit2.http.PUT;
+import retrofit2.http.Path;
+
+/**
+ * Created by ahmaadyunus on 15/12/16.
+ */
+
+public interface UserApi {
+
+    @GET("https://private-80e9a-android23.apiary-mock.com/users")
+    Call<Users> getUsers();
+
+
+    @GET("https://private-80e9a-android23.apiary-mock.com/users/{id}")
+
+    Call<User> getUser(@Path("d") String user_id);
+
+
+    @PUT("https://private-80e9a-android23.apiary-mock.com/users/{id}")
+
+    Call<User> updateUser(@Path("id") int user_id, @Body User user);
+
+
+    @POST("https://private-80e9a-android23.apiary-mock.com/users")
+
+    Call<User> saveUser(@Body User user);
+
+
+    @DELETE("https://private-80e9a-android23.apiary-mock.com/users/{id}")
+
+    Call<User> deleteUser(@Path("id") String user_id);
+
+}
